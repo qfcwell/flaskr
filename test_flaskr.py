c9ff5f3a4adb27431c97aa1c9376a46cf9980011
qfcwell <qfcqfcqfc@qq.com>
2017-09-30 16:43:21 +0800

    2

--- test_flaskr.py
+++ test_flaskr.py
@@ -36,12 +36,12 @@ app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWD')
 
-def seng_async_email(app,msg):
+def send_async_email(app,msg):
     with app.app_context():
         mail.send(msg)
         
 def send_email(to,subject,template,**kwargs):
-    mgs = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
                   sender = app.config['FLASKY_MAIL_SENDER'],recipients = [to])
     msg.body = render_template(template+'.txt',**kwargs)
     msg.html = render_template(template+'.html',**kwargs)
