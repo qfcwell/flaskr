6123e07883f29770212ab8d104f68ce2188fafcb
qfcwell <qfcqfcqfc@qq.com>
2017-10-02 15:24:31 +0800

    1

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+from flask import Flask,render_template,session,redirect,url_for,flash
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail,Message
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    
+    bootstrap.init_app
+    mail.init_app
+    moment.init_app
+    db.init_app
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
